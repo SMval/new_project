83e064fa01c635e7ca1e7fb723864021a751a84a
SMval <127229220+SMval@users.noreply.github.com>
2023-08-01 01:21:04 +0300

    Update_docstring str_func.py

--- str_func.py
+++ str_func.py
@@ -1,15 +1,6 @@
 def get_upp(word):
     """
-    Возращаетстроку с заглавными со всеми заглавными буквами
+    Возвращает строку с заглавными со всеми заглавными буквами
     """
     word_up = word.upper()
     return word_up
-
-
-def get_title(word):
-    """
-    Возвращает строку с заглавными первыми буквами
-    """
-    title_word = word.title()
-    return title_word
-
