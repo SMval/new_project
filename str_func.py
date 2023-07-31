5fd49f31f777a4affbee4400dbba3df18019176f
makarov_s <rvsn3113@mail.ru>
2023-08-01 01:10:28 +0300

    upper and title

--- str_func.py
+++ str_func.py
@@ -1,3 +1,15 @@
 def get_upp(word):
+    """
+    Возращаетстроку с заглавными со всеми заглавными буквами
+    """
     word_up = word.upper()
     return word_up
+
+
+def get_title(word):
+    """
+    Возвращает строку с заглавными первыми буквами
+    """
+    title_word = word.title()
+    return title_word
+
